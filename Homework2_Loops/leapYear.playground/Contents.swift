7212c14ea40b72a0d226c4dd9183d70710c8eab6
IrinaBobarykina <63959254+IrinaBobarykina@users.noreply.github.com>
2023-09-17 15:59:19 +0300

    Update Contents.swift

--- Homework2_Loops/leapYear.playground/Contents.swift
+++ Homework2_Loops/leapYear.playground/Contents.swift
@@ -2,7 +2,21 @@
 
 
 func isLeapYear(year: Int){
-    year % 4 == 0 ? print("Високосный") : print("Невисокосный")
+    if year % 4 != 0 {
+        print("Високосный")
+    }
+    else if year % 100 == 0 {
+        if year % 400 == 0 {
+            print("Високосный")
+        }
+        else{
+            print("Невисокосный")
+        }
+    }
+    else{
+        print("Високосный")
+    }    
+
 }
 
 let currentYear = 2013
